ed7b5c770d8371a679d3f9512e96391cee5a622b
OdeliaFink <odelia.finkel@gmail.com>
2021-02-01 20:16:33 +0000

    linted version

--- tail.js
+++ tail.js
@@ -9,12 +9,12 @@ const assertEqual = function(actual, expected) {
 };
 
 const tail = function(array) {
-  return array.slice(1)
+  return array.slice(1);
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs", "Odelia"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 2); // original array should still have 3 elements!
 assertEqual(words[1], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(words[0], "Odelia"); 
+assertEqual(words[0], "Odelia");
 assertEqual(words[1], "Lighthouse"); // ensure second element is "Lighthouse"
