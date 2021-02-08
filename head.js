c12bde88eaa63256c65945e9fb886fe58c12534b
OdeliaFink <odelia.finkel@gmail.com>
2021-02-08 18:02:30 +0000

    refactored with dry code

--- head.js
+++ head.js
@@ -1,18 +1,7 @@
-const assertEqual = function(actual, expected) {
-  let a = actual;
-  let b = expected;
-  if (a === b) {
-    console.log(`✅ Assertion Passed: ${a} === ${b}`);
-  } else if (a !== b) {
-    console.log(`🛑 Assertion Failed: ${a} !== ${b}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Palm tree", "Maple tree"]), "Cedar tree");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([2,3,4,5]), 1);
+module.exports = head;
