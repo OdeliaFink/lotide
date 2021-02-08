f9df67029034e207ce176e37b4e2c5a51e1639f9
OdeliaFink <odelia.finkel@gmail.com>
2021-02-08 19:31:04 +0000

    added chai mocha updated

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,17 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual')
 const head = require('../head')
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Palm tree", "Maple tree"]), "Cedar tree");
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([2,3,4,5]), 1);
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
